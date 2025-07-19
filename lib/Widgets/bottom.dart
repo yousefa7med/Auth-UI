21a586602144cff56f0d3fdbb229db98f13819c8
yousefa7med <youssefahmedserag@gmail.com>
2025-07-19 18:56:52 +0300

    add costum bottom

---
+++ lib/Widgets/bottom.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  const Bottom({super.key, required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xff9C28B2),
+          borderRadius: BorderRadius.circular(45),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

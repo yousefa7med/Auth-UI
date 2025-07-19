152ca4301ddb2f0c5242cfa4e120068770ba5b55
yousefa7med <youssefahmedserag@gmail.com>
2025-07-19 18:34:22 +0300

    costum text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth_ui/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-
+      home: const LoginPage(),
     );
   }
 }
